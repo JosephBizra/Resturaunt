afcbd59a0d392ba82a0417a9e184b61a6f883bc5
David Garza <dgarza0413@gmail.com>
2019-07-03 12:45:25 -0500

    basic server routes in place

---
+++ server.js
@@ -0,0 +1,50 @@
+// dependencies
+
+var express = require("express")
+var path = require("path")
+
+var app = express();
+var PORT = 3000;
+
+
+
+var reservations = [
+    {
+        ID: "afhaque89",
+        Name: "Ahmed",
+        Email: "ahmed@example.com",
+        Phone: "000 - 000 - 0000"
+    },
+    {
+        ID: "dgarza",
+        Name: "david garza",
+        email: "dg@mail.com",
+        phone: "000 - 123 - 4566"
+    },
+    {
+        ID: "bMc",
+        Name: "david garza",
+        email: "dg@mail.com",
+        phone: "000 - 123 - 4566"
+    }
+];
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/add", function (req, res) {
+    res.sendFile(path.join(__dirname, "add.html"))
+})
+
+app.get("/all", function (req, res) {
+    res.sendFile(path.join(__dirname, "all.html"))
+})
+
+app.get("/api/reservations", function (req, res) {
+    return res.json(reservations)
+})
+
+app.listen(PORT, function () {
+    console.log("App is listening on PORT: " + PORT);
+})
