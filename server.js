5240d010740e8a5d571d29f7db714ea99a0f39b9
David Garza <dgarza0413@gmail.com>
2019-07-03 12:52:36 -0500

    server update

--- server.js
+++ server.js
@@ -45,6 +45,7 @@ app.get("/api/reservations", function (req, res) {
     return res.json(reservations)
 })
 
+//listen on the port difference
 app.listen(PORT, function () {
     console.log("App is listening on PORT: " + PORT);
 })
